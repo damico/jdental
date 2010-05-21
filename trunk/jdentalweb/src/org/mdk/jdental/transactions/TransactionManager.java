86cb420973dcf82b3af66178bffffb36bcd3b689
jd.comment <jd.comment@1e5372cd-2898-3f5b-2359-99c5e8b371c0>
2010-05-21 17:54:24 +0000

    re-creation of Transaction manager

--- trunk/jdentalweb/src/org/mdk/jdental/transactions/TransactionManager.java
+++ trunk/jdentalweb/src/org/mdk/jdental/transactions/TransactionManager.java
@@ -1,10 +1,24 @@
 package org.mdk.jdental.transactions;
 
+import org.mdk.jdental.utils.Constants;
+import org.mdk.jdental.utils.ManageProperties;
+
 public class TransactionManager {
 
 	public DatabaseAdaptor getDbAdaptor() {
-		// TODO Auto-generated method stub
-		return null;
+		DatabaseAdaptor ret = null;
+		String classfn = ManageProperties.getInstance().read(Constants.DB_CLASSFN);
+		if(classfn.equals("org.postgresql.Driver")){
+			String dbhost = ManageProperties.getInstance().read(Constants.DB_HOST);
+			String dbport = ManageProperties.getInstance().read(Constants.DB_PORT);
+			String dbname = ManageProperties.getInstance().read(Constants.DB_NAME);
+			String dbuser = ManageProperties.getInstance().read(Constants.DB_USER);
+			String dbpasswd = ManageProperties.getInstance().read(Constants.DB_PASSWD);
+			
+			DatabaseConfig dConfig = new DatabaseConfig(classfn, dbhost, dbport, dbname, dbuser, dbpasswd);
+			ret = new PostgresManager(dConfig);
+		}
+		return ret;
 	}
 
 }
