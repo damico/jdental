78f78c3f3f53142a7061001a726fc023170135ac
jd.comment <jd.comment@1e5372cd-2898-3f5b-2359-99c5e8b371c0>
2010-06-24 19:42:05 +0000

    refactoring, changing jdental by netterklinik

--- trunk/jdentalweb/src/org/mdk/jdental/utils/Constants.java
+++ trunk/jdentalweb/src/org/mdk/jdental/utils/Constants.java
@@ -10,14 +10,14 @@ public interface Constants {
 
 	public static final String RENDER_SERVLET_FILE_PARAM = "file";
 
-	public static final String DEFAULT_CONTENTPKG_PATH = "/opt/jdental/";
+	public static final String DEFAULT_CONTENTPKG_PATH = "/opt/netterklinik/";
 
 	public static final String APP_VERSION = "0.0.1";
 
-	public static final String APP_NAME = "jDental";
+	public static final String APP_NAME = "app.name";
 
 	/* conf file */
-	public static final String PROPERTIES_PATH = "/etc/jdental/jdental.conf";
+	public static final String PROPERTIES_PATH = "/etc/netterklinik/netterklinik.conf";
 	
 	
 	public static final String DB_CLASSFN = "classfn";
@@ -28,9 +28,9 @@ public interface Constants {
 	public static final String DB_PASSWD = "dbpasswd";
 	public static final String DB_PORT = "dbport";
 	
-	public static final String APP_FOLDER = "/opt/jdental/";
+	public static final String APP_FOLDER = "/opt/netterklinik/";
 
-	public static final String LOG_NAME = "jdental.log";
+	public static final String LOG_NAME = "netterklinik.log";
 	
 	public static final String SEVERE_LOGLEVEL = " S ";
 	public static final String NORMAL_LOGLEVEL = " N ";
@@ -40,7 +40,7 @@ public interface Constants {
 
 	public static final String SQL_GETFEELING_DERBY = "SELECT COUNT(EMOTION) FROM RSSF.URI WHERE TIMESTAMP >= ? AND EMOTION = ?";
 
-	public static final String LOG_FOLDER = "/var/log/jdental/";
+	public static final String LOG_FOLDER = "/var/log/netterklinik/";
 
 	public static final String DEFAULT_DATETIME_FORMAT = "dd/mm/yyyy H:m:s";
 
